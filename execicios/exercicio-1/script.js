4ace33a3cb3f780ae9822b171ed5e235423bfae3
Yuri Thairony <yurithairony@hotmail.com>
2023-10-28 18:30:49 -0300

    Exercio 1 proposto

---
+++ execicios/exercicio-1/script.js
@@ -0,0 +1,21 @@
+
+function carregar() {
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('img')
+
+    let data = new Date()
+    // let hora = data.getHours()
+    let hora = 12
+    msg.innerHTML = `Agora são ${hora} horas`
+    
+    if (hora >= 0 && hora < 12) {
+        document.body.style.background = '#C8DBF1'
+        img.style.backgroundImage = 'url(fotos/manha.jpg)';
+    } else if (hora >= 12 && hora < 18) {
+        document.body.style.background = '#EAE3D9'
+        img.style.backgroundImage = 'url(fotos/tarde.jpg)';
+    } else {
+        document.body.style.background = '#33340B'
+        img.style.backgroundImage = 'url(fotos/noite.jpg)';
+    }
+}
